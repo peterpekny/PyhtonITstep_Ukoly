08e0b3cf47ec5426ac17f5f7f86de0d9aff3bae1
Peter Pekny <113863616+peterpekny@users.noreply.github.com>
2025-01-05 15:36:00 +0100

    update web generator

--- 09/page_generator.py
+++ 09/page_generator.py
@@ -1,12 +1,40 @@
 import json
 
 # basic template for generate:
+
+# a_element = """
+#             <div class="card" style="width: 10rem;">
+#                 <img src="{img}" class="img-thumbnail" alt="{title}">
+#                 <div class="card-body">
+#                     <h5 class="card-title">{title}</h5>
+#                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+#                     <a href="{url}" class="btn btn-primary">Click Here</a>
+#                 </div>
+#             </div>
+#             """
+# a_element = """
+#             <div class="card">
+#                 <img src="{img}" class="card-img-top" alt="{title}">
+#                 <div class="card-body">
+#                 <h5 class="card-title">{title}</h5>
+#                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+#                 </div>
+#                 <div class="card-footer">
+#                 <small class="text-muted">Last updated 3 mins ago</small>
+#                 </div>
+#             </div>
+#             """
+
 a_element = """
-            <a href="{url}">
-                <h2>{title}</h2>
-                <img src="{img}" alt="{title}">
-            </a>
-            """
+<div class="mycard" style="width: 10rem;"> 
+    <img src="{img}" class="thumbnail" alt="{title}"> 
+    <div class="cardbody"> 
+        <h5 class="cardtitle">{title}</h5> 
+        <p class="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+        <a href="{url}" class="btn btn-primary">Click Here</a>
+    </div>
+</div>
+"""
 
 def gen_list():
     # load data from json:
