5b30bdd0e79e44e267361c2d42ea3faa7a14abbe
Peter Pekny <113863616+peterpekny@users.noreply.github.com>
2025-01-15 21:57:41 +0100

    updated functions for unittests

--- 15/get_upper_chars.py
+++ 15/get_upper_chars.py
@@ -2,4 +2,9 @@ def get_upper_chars(text):
     """ z textu vrací pouze znaky, které jsou velkými písmeny """
     """ např: Ahoj jsem teď v Praze -> vrátí pouze AP """
     
-    return ''.join([char for char in text if char.isupper()])
+    upperchars = ''
+    for char in text:
+        if char.isupper():
+            upperchars += ''.join(char)
+    
+    return upperchars
